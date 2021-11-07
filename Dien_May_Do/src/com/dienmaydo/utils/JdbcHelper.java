766ca667a6a5dab42b6985dee6839eb53635be0f
Nguyễn Viết Hiên <56214108+nvhct2002vn@users.noreply.github.com>
2021-11-08 00:23:21 +0700

    Update JdbcHelper.java

--- Dien_May_Do/src/com/dienmaydo/utils/JdbcHelper.java
+++ Dien_May_Do/src/com/dienmaydo/utils/JdbcHelper.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class JdbcHelper {
 
-    private static String url = "jdbc:sqlserver://localhost;database = QLBH_DIENMAYDO";
+    private static String url = "jdbc:sqlserver://localhost;database = QLBHDIENMAY_DO";
     private static String usename = "sa";
     private static String password = "123";
 
