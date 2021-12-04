8a129d45467d7b23385dccc2456e55db22cdb4f9
Hoadt02 <dotathoa2002@gmail.com>
2021-12-04 09:01:44 +0700

    hòa up

--- Dien_May_Do/src/com/dienmaydo/utils/NewClass.java
+++ Dien_May_Do/src/com/dienmaydo/utils/NewClass.java
@@ -5,11 +5,23 @@
  */
 package com.dienmaydo.utils;
 
+import java.util.Date;
+
 /**
  *
  * @author DO TAT HOA
  */
 public class NewClass {
 
-
+    public static void main(String[] args) {
+        Date hienTai = XDate.toDate("2021-12-04");
+        Date batDau = XDate.toDate("2021-12-04");
+        Date ketThuc = XDate.toDate("2021-12-10");
+        
+        if (hienTai.before(ketThuc)) {
+            System.out.println("Còn hoạt đọng");
+        }else{
+            System.out.println("Không hoạt động");
+        }
+    }
 }
