11950e0ee742aa0bface49a0337d0a0a1016a723
Hoadt02 <dotathoa2002@gmail.com>
2021-12-09 20:48:32 +0700

    hòa đẩy code cuối cùng

--- Dien_May_Do/src/com/dienmaydo/gui/KhachHangJframe.java
+++ Dien_May_Do/src/com/dienmaydo/gui/KhachHangJframe.java
@@ -109,6 +109,7 @@ public class KhachHangJframe extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
+        tblQuanLyKhacHang.setRowHeight(20);
         tblQuanLyKhacHang.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 tblQuanLyKhacHangMouseClicked(evt);
